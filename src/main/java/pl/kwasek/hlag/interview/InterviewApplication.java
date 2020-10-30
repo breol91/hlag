55740909b880db30b1e303f4a52dff5dafdae352
Rafal Kwasek <rafal.kwasek@innokrea.pl>
2020-10-30 12:50:04 +0100

    add Application class

---
+++ src/main/java/pl/kwasek/hlag/interview/InterviewApplication.java
@@ -0,0 +1,8 @@
+package pl.kwasek.hlag.interview;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class InterviewApplication extends Application {
+
+}
